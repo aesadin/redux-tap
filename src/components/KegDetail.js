f209981d221d40a0bdddf72876b713bf4afca5fe
Allison Sadin <aesadin@gmail.com>
2020-09-11 15:35:38 -0700

    populate keg detail component

---
+++ src/components/KegDetail.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+function KegDetail(props){
+  const {keg, onClickingDelete} = props;
+
+  return (
+    <React.Fragment>
+      <h1>Keg Detail</h1>
+      <h3>{keg.brand} - {keg.name}</h3>
+      <p>{keg.price}</p>
+      <p>{keg.alcoholContent}</p>
+      <button onClick={ props.onClickingEdit }>Update Keg</button>
+      <button onClick={()=> onClickingDelete(keg.id) }>Delete Keg</button>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+TicketDetail.propTypes = {
+  ticket: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
+}
+
+export default KegDetail;

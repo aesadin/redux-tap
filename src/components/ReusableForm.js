6fb0785ad49d7b0242ff3b49898f89d26526dfe7
Allison Sadin <aesadin@gmail.com>
2020-09-11 13:36:00 -0700

    create reusable form with all inputs

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props){
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='brand'
+          placeholder='Brand Name' />
+        <input
+          type='text'
+          name='name'
+          placeholder='Beer Name' />
+        <input
+          name='type'
+          placeholder='Type of Beer i.e. IPA, Pilsner, Amber' />
+        <input
+          type='price'
+          name='price'
+          placeholder='Keg Price' />
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;

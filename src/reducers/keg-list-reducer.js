d48a01dbadaaa525ac777275269ed06a411092d7
Allison Sadin <aesadin@gmail.com>
2020-09-18 14:28:08 -0700

    passed test

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,4 +1,24 @@
 
 export default (state = {}, action) => {
-  
+  const { brand, name, type, price, pintsInKeg, alcoholContent, id } = action;
+  switch (action.type) {
+  case 'ADD_KEG':
+    return Object.assign({}, state, {
+      [id]: {
+        brand: brand,
+        name: name,
+        type: type,
+        price: price,
+        pintsInKeg: pintsInKeg,
+        alcoholContent: alcoholContent,
+        id: id
+      }
+    });
+  case 'DELETE_KEG':
+    const newState = { ...state};
+    delete newState[id];
+    return newState;
+  default:
+    return state;
+  }
 }
